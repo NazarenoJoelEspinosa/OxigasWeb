9bd42696cff6392688f6c9986eb5b29abb115824
NazarenoJoelEspinosa <nazarenojoelespinosa@gmail.com>
2026-06-04 16:53:49 -0300

    A

--- oxi-gas/src/main.tsx
+++ oxi-gas/src/main.tsx
@@ -2,6 +2,21 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Restaurar rutas en GitHub Pages
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+
+  const base = import.meta.env.BASE_URL.replace(/\/$/, "");
+
+  window.history.replaceState(
+    null,
+    "",
+    `${base}${redirect}`
+  );
+}
+
 createRoot(document.getElementById("root")!).render(
   <App />
 );
